bd48778df01150774804cdb82ff432b02c334cc0
moretto1 <bmoretto404@gmail.com>
2021-09-02 22:29:18 -0300

    Added validation if task page existis on a task creation

--- src/main/java/moretto/bruno/schedule/services/TaskPageService.java
+++ src/main/java/moretto/bruno/schedule/services/TaskPageService.java
@@ -2,8 +2,6 @@ package moretto.bruno.schedule.services;
 
 import moretto.bruno.schedule.domain.dtos.TaskPageDto;
 
-import java.util.List;
-
 public interface TaskPageService {
 
     void create(TaskPageDto taskPageDto);
--- src/main/java/moretto/bruno/schedule/services/impl/TaskServiceImpl.java
+++ src/main/java/moretto/bruno/schedule/services/impl/TaskServiceImpl.java
@@ -2,12 +2,18 @@ package moretto.bruno.schedule.services.impl;
 
 import moretto.bruno.schedule.domain.bo.TaskBo;
 import moretto.bruno.schedule.domain.dtos.TaskDto;
+import moretto.bruno.schedule.domain.dtos.TaskPageDto;
 import moretto.bruno.schedule.domain.entities.Task;
+import moretto.bruno.schedule.domain.entities.TaskPage;
 import moretto.bruno.schedule.domain.mappers.Mapper;
 import moretto.bruno.schedule.repositories.TaskRepository;
+import moretto.bruno.schedule.services.TaskPageService;
 import moretto.bruno.schedule.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -19,13 +25,22 @@ public class TaskServiceImpl implements TaskService {
     private TaskBo taskBo;
 
     @Autowired
+    private TaskPageService taskPageService;
+
+    @Autowired
     private Mapper<TaskDto, Task> mapper;
 
     @Override
     public void create(TaskDto taskDto) {
         Task task = mapper.mapToEntity(taskDto, Task.class);
+        checkIfExistsTaskPage(taskDto.getTaskPageId());
         taskBo.setCommonsData(task);
         taskRepository.save(task);
+    }
+
+    private boolean checkIfExistsTaskPage(Long taskPageId) {
+        var taskPage = taskPageService.findById(taskPageId);
+        return Objects.nonNull(taskPage);
     }
 
 }
